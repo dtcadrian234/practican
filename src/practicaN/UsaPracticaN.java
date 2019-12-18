0d76ddfe711db5fd1205532547ce045365b73a8d
dtcad <dtcad@DESKTOP-MBPOSD5>
2019-12-17 20:42:13 -0500

    Es una practica con un boton.

---
+++ src/practicaN/UsaPracticaN.java
@@ -0,0 +1,16 @@
+
+package practicaN;
+
+/**
+ *
+ * @author dtcad
+ */
+public class UsaPracticaN {
+    
+    public static void main(String[] args) {
+        PracticaN p = new PracticaN();
+        p.setVisible(true);
+        p.setLocationRelativeTo(null);
+        
+    }
+}
